eb2609091b8afa8b2180fcc0b5fa39104d4494a9
Eric Samuel Schneider <eric.schneider@stu.jefferson.kyschools.us>
2018-09-24 08:17:13 -0400

    Finished product

--- dictionary project.py
+++ dictionary project.py
@@ -1,43 +1,43 @@
 #Eric Schneider
-#dictionaries 9/17/18-
+#dictionaries
+#9/17/18-9/24/18
 import random
 import time
+import sys
 
 #Baseline stats
-xp=0
-strengh=random.choice(range(5,13))
-magics=random.choice(range(5,13))
-dexteriti=random.choice(range(5,13))
-defence=random.choice(range(5,13))
+strengh=random.choice(range(5,16))
+magics=random.choice(range(5,16))
+dexteriti=random.choice(range(8,16))
+names= input('Please enter the name you wish to be called by.')
 hp=dexteriti*3
 
-
 #Dictionary
-def view_player(Name, Xp, Level, Strength, Magic, Dexterity, Hp, Defense):
+#stat inputs
+player={
+    'Name':names,
+    'Strength':strengh,
+    'Magic':magics,
+    'Dexterity':dexteriti,
+    'Health':dexteriti*3,
+    }
 
-    #stat inputs
-    player={
-        'Name':names,
-        'Xp':xp,
-        'Level':1,
-        'Strength':strengh,
-        'Magic':magics,
-        'Dexterity':dexteriti,
-        'Hp':hp,
-        'Defense':defence,
-        }
-    return player
+#because of variable location, i was unable to add the monster attack system into a command.
+#I had to put both remaining hp values into the same command meaning that they would reset each turn.
+#In the future I would like to figure out if there is a way to fix that problem.
 
-#very long and very conveluted encouter with a single monster
+#encouter with a single monster
 def encounter():
     print ('A wild ork stands in your path, there is no turning around and no running away. you must fight.')
     ork_hp=90
+    player_hp=player.get('Health')
+    
     while ork_hp>=0:
         attack= input('How will you attack? With [m]agic or with a [s]word?')
         
     #Magic path
         if attack=='m':
-            spell= input ('What spell? [r]ay of frost: 90% accuracy,[f]lair bolt: 90% accuracy, [q]uicksand: 35% accuracy, but stops the monster from attacking, [m]agic Missile: 50% accuracy, but does double damage.')
+            spell= input ('What spell? [r]ay of frost: 90% accuracy,[f]lair bolt: 90% accuracy, [q]uicksand: 45% accuracy, but stops the monster from attacking, [m]agic Missile: 60% accuracy, but does double damage.')
 
     #Spell:Ray of Frost
             if spell=='r':
@@ -48,13 +48,29 @@ def encounter():
                     print('You do %s'%damage +' points of damage.')
                     ork_hp-=damage
 
-                    monster_attack()
-
+                    time.sleep(2)
+                    player_hp-=random.choice(range(1,6))
+                    print('Now the ork will attack')
+                    if player_hp<=0:
+                        print('you died')
+                        print('you lose')
+                        sys.exit()
+                    else:
+                        print('You have %s'% player_hp +' points of health remaining')
+                    
                 else:
                     print('your attack misses')
-                    time.sleep(5)
 
-                    monster_attack()
+                    time.sleep(2)
+
+                    player_hp-=random.choice(range(1,6))
+                    print('Now the ork will attack')
+                    if player_hp<=0:
+                        print('you died')
+                        print('you lose')
+                        sys.exit()
+                    else:
+                        print('You have %s'% player_hp +' points of health remaining')
                     
     #Spell:Flair Bolt            
             elif spell=='f':
@@ -65,49 +81,84 @@ def encounter():
                     print('You do %s'%damage +' points of damage')
                     ork_hp-=damage
 
-                    monster_attack()
-                    
+                    time.sleep(2)
+                    player_hp-=random.choice(range(1,6))
+                    print('Now the ork will attack')
+                    if player_hp<=0:
+                        print('you died')
+                        print('you lose')
+                        sys.exit()
+                    else:
+                        print('You have %s'% player_hp +' points of health remaining')
+            
                 else:
                     print('your attack misses')
-                    time.sleep(5)
 
-                    monster_attack()
+                    time.sleep(2)
+                    player_hp-=random.choice(range(1,6))
+                    print('Now the ork will attack')
+                    if player_hp<=0:
+                        print('you died')
+                        print('you lose')
+                        sys.exit()
+                    else:
+                        print('You have %s'% player_hp +' points of health remaining') 
                     
     #Spell:Quicksand         
             elif spell=='q':
                 d20=random.choice(range(1,21))
-                if d20>=8:
+                if d20>=10:
                     print('You wave your hands in front of you, turning the ground under the monster into sand')
                     damage=magics
-                    print('You do %s'%damage %'%s points of damage')
+                    print('You do %s'%damage +' points of damage')
                     ork_hp-=damage
 
                     print('The ork is stuck in quicksand and can not attack')
                     
                 else:
                     print('your attack misses')
-                    time.sleep(5)
-
-                    monter_attack()
+                    time.sleep(2)
+                    player_hp-=random.choice(range(1,6))
+                    print('Now the ork will attack')
+                    if player_hp<=0:
+                        print('you died')
+                        print('you lose')
+                        sys.exit()
+                    else:
+                        print('You have %s'% player_hp +' points of health remaining')
                     
-    #Spell:Magic Missle   
+    #Spell:Magic Missile   
             elif spell=='m':
                 d20=random.choice(range(1,21))
-                if d20>=11:
+                if d20>=13:
                     print('You create three glowing darts of magical force and hurl them at the creature.')
-                    damage=magics
-                    print('You do %s'%damage %'%s points of damage')
+                    damage=magics*2
+                    print('You do %s'%damage +' points of damage')
                     ork_hp-=damage*2
-                    time.sleep(5)
 
-                    monster_attack()
+                    time.sleep(2)
+                    player_hp-=random.choice(range(1,6))
+                    print('Now the ork will attack')
+                    if player_hp<=0:
+                        print('you died')
+                        print('you lose')
+                        sys.exit()
+                    else:
+                        print('You have %s'% player_hp +' points of health remaining') 
                     
                 else:
                     print('your attack misses')
-                    time.sleep(5)
 
-                
-                    monster_attack()
+                    time.sleep(2)
+                    
+                    player_hp-=ork_damage
+                    print('Now the ork will attack')
+                    if player_hp<=0:
+                        print('you died')
+                        print('you lose')
+                        sys.exit()
+                    else:
+                        print('You have %s'% player_hp +' points of health remaining') 
                     
             else:
                 print('Please enter a correct input.')
@@ -115,75 +166,70 @@ def encounter():
     #Melee path
         elif attack=='s':
             print('You brandish your sword.')
-            time.sleep(3)
             d10=random.choice(range(1,11))
             damage=strengh+d10
             print('you do %s'%damage+' points of damage')
             ork_hp-=damage
-            time.sleep(5)
 
-
-            monster_attack()
-            
+            time.sleep(2)
+            player_hp-=random.choice(range(1,6))
+            print('Now the ork will attack')
+            if player_hp<=0:
+                print('you died')
+                print('you lose')
+                sys.exit()
+            else:
+                print('You have %s'% player_hp +' points of health remaining')
+                          
         else:
             print('Please enter a correct input')
             
     return "you Win"
 
-
-def monster_attack():
-    print('Now the ork will attack')
-    ork_damage=7-defence/2
-    time.sleep(5)
-    print('You take %s'% ork_damage +' damage')
-    player_hp-=ork_damage
-    time.sleep(5)
-    print('You have %s'% hp %'%s points of health remaining')
-
-#player_hp=view_player[hp]
- 
 #Start up screen
-names= input('Please enter the name you wish to be called by.')
-#time.sleep(5)
-print(view_player('name', 'xp', 'level', 'strengh', 'magics', 'dexteriti', 'hp', 'defence'))
-#time.sleep(5)
-print ('you will see your stats in the future whenever you level up.')
-#time.sleep(5)
+
+time.sleep(2)
+print (player)
+time.sleep(2)
 print('You can play for as long as you wish to, but can only stop after you win a round or die.')
-#time.sleep(5)
+time.sleep(2)
 print('Before we begin playing, I must first tell you a couple ground rules.')
-#time.sleep(5)
+time.sleep(2)
 print('every time you sustain a hit, you will take damage and your remaining hp will be shown.')
-#time.sleep(5)
-print('You will automatically heal to your max health when you level up.')
-#time.sleep(5)
-print('Your ammount of dammage is directly proportional to your strength stat.')
-#time.sleep(5)
+time.sleep(2)
+print('You will automatically heal to your max health when you win the encounter, but if you die its over.')
+time.sleep(2)
+print('Your ammount of damage is directly proportional to your strength stat.')
+time.sleep(2)
 print('The same is true your magic stat.')
-#time.sleep(5)
-print('Half of your defence is equal to the amount of damage that will not affect you.')
-#time.sleep(5)
-print('And finally, your Health is equal to three times yur dexterity.')
-#time.sleep(5)
+time.sleep(2)
+print('And finally, your Health is equal to three times your dexterity.')
+time.sleep(2)
 print('Now lets start this game.')
 replay=''
+
 #game
-
-while replay.upper!='N':
-
+while replay.upper!='n':
     encounter()
-    print('you see 200 xp rise from the monsters dead corpse. without thinking of how bad an idea it is though, you take it.')
-    xp+=200
-    if xp>100:
-        print('you leveled up')
-        strengh+=random.choice(range(5,13))
-        magics+=random.choice(range(5,13))
-        dexteriti+=random.choice(range(5,13))
-        defence+=random.choice(range(5,13))
-        hp=dexteriti*3
-        print('Your stats increase')
-        print(view_player('name', 'xp', 'level', 'strengh', 'magics', 'dexteriti', 'hp', 'defence'))
+    time.sleep(3)
+    print('the monster stumbles back,defeated.')
+    time.sleep(2)
+    print('You gain 200 xp from your valiant battle')
+    time.sleep(2)
+    print('you feel power enter your body, but nothing happens')
+    time.sleep(2)
+    replay= input('Do you want to continue? [y]es or [n]o')
+    if replay!='n':
+        print('Yay, we get to play again.')
+        time.sleep(2)
+        print('Here are your stats again.')
+        time.sleep(2)
+        print(player)
     else:
+        print('Okay, I totally understand. Come back and play with me sometimes. It can get pretty lonely sitting in this file alone for all eternity.')
         time.sleep(5)
-    replay= input('Do you want to continue? [Y] or [N]')
-    
+        print('Please dont leave me alone forever. You dont know what its like to be forgotten')
+        time.sleep(5)
+print('Goodbye')
+time.sleep(5)
+print('And remember, deleating a file is murder.')
